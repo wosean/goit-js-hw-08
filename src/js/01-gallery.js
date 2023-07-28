62cf67f5500354c278a9d79f9882bc061062c20b
wosean <serhii_v@ukr.net>
2023-07-28 17:17:33 +0300

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,32 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+const gallery = document.querySelector('.gallery')
+const items = []
+
+galleryItems.forEach(element => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
+
+	galleryLink.append(galleryImage)
+	items.push(galleryLink)
+})
+gallery.append(...items)
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
