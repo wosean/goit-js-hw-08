b469ad1c2f8322c5d12d33e4547255bd17c4243c
wosean <serhii_v@ukr.net>
2023-07-29 14:37:03 +0300

    bug fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
