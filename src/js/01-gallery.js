26687362196ad8782194d9a00f6efbc949d415fd
wosean <serhii_v@ukr.net>
2023-07-31 21:18:00 +0300

    updated task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,34 +1,60 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// // Описаний в документації
+// import SimpleLightbox from "simplelightbox";
+// // Додатковий імпорт стилів
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
+// const gallery = document.querySelector('.gallery')
+// const items = []
+
+// galleryItems.forEach(element => {
+// 	const galleryLink = document.createElement('a')
+// 	galleryLink.className = 'gallery__link'
+// 	galleryLink.href = element.original
+// 	const galleryImage = document.createElement('img')
+// 	galleryImage.className = 'gallery__image'
+// 	galleryImage.src = element.preview
+// 	galleryImage.setAttribute('title', element.description)
+// 	galleryImage.alt = element.description
+
+// 	galleryLink.append(galleryImage)
+// 	items.push(galleryLink)
+// })
+// gallery.append(...items)
+
+// new SimpleLightbox('.gallery a', {
+// 	captionDelay: 250
+// })
+
+
+// v.2
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// console.log(galleryItems);
 
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryMarkUp = document.querySelector('.gallery');
 
+const galleryEl = galleryItems
+    .map(({ preview, description, original }) =>
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+			 src="${preview}" alt="${description}" />
+        </a>`
+).join('');
 
-const gallery = document.querySelector('.gallery')
-const items = []
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
 
-galleryItems.forEach(element => {
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-	galleryImage.className = 'gallery__image'
-	galleryImage.src = element.preview
-	galleryImage.setAttribute('title', element.description)
-	galleryImage.alt = element.description
-
-	galleryLink.append(galleryImage)
-	items.push(galleryLink)
-})
-gallery.append(...items)
-
-new SimpleLightbox('.gallery a', {
-	captionDelay: 250
-})
+const lightbox = new SimpleLightbox('.gallery a',
+	{ captionsData: "alt", captionDelay: 250, captionPosition: "bottom", });
