5c328830bc2794dbaffea4599f10047f6827e0da
wosean <serhii_v@ukr.net>
2023-07-31 17:53:56 +0300

    updata task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,30 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate',  throttle( e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
-    }, 1000)
-);
+const CURRENT_TIME_KEY = `video-player-timekey`;
+function getCurrentTimeKey(timeKey){
+    localStorage.setItem(CURRENT_TIME_KEY, timeKey.second);
+}
+player.on('timeupdate', throttle(getCurrentTimeKey, 1000));
+
+const currentTimeValue = localStorage.getItem(CURRENT_TIME_KEY || 0);
+player.setCurrentTime(currentTimeValue);
+
+
+// v.1
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate',  throttle( e => {
+//     localStorage.setItem('videoplayer-current-time', e.seconds);
+//     }, 1000)
+// );
     
-player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-.catch(function (error) {
-    console.error(error)
-});
+// player
+// .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+// .catch(function (error) {
+//     console.error(error)
+// });
